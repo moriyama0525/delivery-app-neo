6e0327ce0ded5a1ca5bcdc828c646485ddfe9f55
takamasamoriyama <ymca0525a@gmail.com>
2020-03-01 17:09:49 +0900

    background-image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
   # Do not eager load code on boot.
   config.eager_load = false
+  config.assets.compile = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Show full error reports.
   config.consider_all_requests_local = true
