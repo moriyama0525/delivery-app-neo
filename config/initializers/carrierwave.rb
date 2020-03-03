533fbb8ade548cf5b81c249fc8bfee15f3644f08
takamasamoriyama <ymca0525a@gmail.com>
2020-03-03 12:56:53 +0900

    change IAMkey

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,1 +1,13 @@
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
 
+    config.fog_directory  = 'health-and-muscle'
+    config.cache_storage = :fog
+  end
+end
